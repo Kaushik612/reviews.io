ffcabfd3e27075a601e1f5d42207a432d16b0564
Kaushik Ravikumar <kaushikr.612@gmail.com>
2023-09-08 10:58:50 -0400

    update google key

--- components/google/GooglePlacesScript.tsx
+++ components/google/GooglePlacesScript.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Script from "next/script";
 
-const source =
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDnnF5xG9h_ZAudsyFVqhs13xgJqetsj0k&libraries=places&callback=Function.prototype";
+let GOOGLE_API_KEY = "";
+const source = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&libraries=places&callback=Function.prototype`;
 
 const GooglePlacesScript = () => {
   return (
